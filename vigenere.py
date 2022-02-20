947f7769da3fdd39c5759834704a8f31583f2e4e
kimiekomi <kimiekomi1@gmail.com>
2022-02-20 14:57:46 -0700

    added encode() to vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -1,6 +1,42 @@
 debug = False
 
-def crack(message, key):
+def encode(message, key):
+
+    if debug: print(f"crack(message='{message}', key='{key}')")
+
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    key_index = 0
+
+    new_message = ""
+
+    for i in range(len(message)):
+
+        if debug: print(message[i])
+            
+        if not message[i].isalpha():
+            new_message += message[i]
+            continue
+        
+        if debug: print(f"key_index='{key_index}'")
+
+        new_index = letters.index(message[i]) + letters.index(key[key_index])
+
+        if new_index > 25:
+            new_index -= 26
+
+        new_message += letters[new_index]
+
+        key_index += 1
+
+        if key_index >= len(key):
+            key_index = key_index % len(key)
+        
+        if debug: print(new_message)
+
+    return new_message
+
+
+def decode(message, key):
 
     if debug: print(f"crack(message='{message}', key='{key}')")
 
@@ -37,7 +73,8 @@ def crack(message, key):
 
 
 if __name__ == "__main__":
-    print(crack("dfc aruw fsti gr vjtwhr wznj? vmph otis! cbx swv jipreneo uhllj kpi rahjib eg fjdkwkedhmp!", "friends"))
+    print(decode("dfc aruw fsti gr vjtwhr wznj? vmph otis! cbx swv jipreneo uhllj kpi rahjib eg fjdkwkedhmp!", "friends"))
+    print(encode("you were able to decode this? nice work! you are becoming quite the expert at crytography!", "friends"))
     
 
 
