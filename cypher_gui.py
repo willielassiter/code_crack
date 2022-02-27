3f14b6eb993dc04c8e9669f65dc977d0da439484
willielassiter <willielassiter@yahoo.com>
2022-02-27 13:44:33 -0800

    Update cypher_gui.py

--- cypher_gui.py
+++ cypher_gui.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
 
 import json
 
@@ -28,263 +30,286 @@ class Cypher_Application (Tk_Application):
 
     def __init__(self, title, width, height, *args, **kwargs):
 
-        super().__init__(self, *args, **kwargs)
+        super().__init__(title, width, height, *args, **kwargs)
 
-        # Text Boxes Labels, Entries, and Buttons
-        self.message = LabelFrame(self, text="Message")
-        self.message.pack(fill="both", expand="yes", padx=20, pady=(20, 0))
+        # Messages: Labels, Entries, and Buttons
+        self.message = LabelFrame(self, text="Messages")
+        self.message.grid(row=0, column=0, padx=20, pady=(20,10))
 
-        self.input_label = Label(self.message, text="Input:")
+        self.input_label = Label(self.message, text="Input Message -")
         self.input_label.grid(row=0, column=0, padx=10, pady=(20,0), sticky=W)
+
         self.input_box = Text(self.message, width=100, height=15)
         self.input_box.grid(row=1, column=0, padx=10, pady=(10,20))
 
-        self.output_label = Label(self.message, text="Output:")
+        self.output_label = Label(self.message, text="Output Messsage -")
         self.output_label.grid(row=2, column=0, padx=10, pady=(10,0), sticky=W)
+
         self.output_box = Text(self.message, width=100, height=15)
-        self.output_box.grid(row=3, column=0, padx=10, pady=(10,0))
+        self.output_box.grid(row=3, column=0, padx=10, pady=(10,20))
 
-        # Options Labels, Entries, and Buttons
-
+        # Options: Labels, Entries, and Buttons
         self.options = LabelFrame(self, text="Options")
-        self.options.pack(fill="x", expand="no", padx=20, pady=20, ipadx=20, ipady=10)
+        self.options.grid(row=1, column=0, padx=20, pady=(0,10), ipadx=4)
 
-        self.offset_keyword = Label(self.options, text="Offset/Keyword")
-        self.offset_keyword.grid(row=0, column=0, padx=20, pady=(15,0), sticky=E)
+        self.button_options = StringVar()
 
-        self.offset_keyword_entry = Entry(self.options, width=10)
-        self.offset_keyword_entry.grid(row=0, column=1, pady=(15,0), sticky=W)
+        self.caesar_button = Radiobutton(self.options, text="Caesar", variable=self.button_options, value="caesar", command=self.disable_keyword)
+        self.caesar_button.grid(row=0, column=0, padx=(18,10), pady=(10,0), sticky=N)
+        self.caesar_button.select()
 
-        self.cypher_option = StringVar()
+        self.vigenere_button = Radiobutton(self.options, text="Vigenere", variable=self.button_options, value="vigenere", command=self.disable_offset)
+        self.vigenere_button.grid(row=1, column=0, padx=(25,10), pady=(0,10), sticky=N)
 
-        self.caesar_button = Radiobutton(self.options, text="Caesar", variable=self.cypher_option, value="caesar")
-        self.caesar_button.grid(row=0, column=2, padx=(18,10), pady=(20,0), sticky=N)
+        self.offset = IntVar()
 
-        self.vigenere_button = Radiobutton(self.options, text="Vigenere", variable=self.cypher_option, value="vigenere")
-        self.vigenere_button.grid(row=0, column=3, padx=(0,10), pady=(20,0), sticky=N)
+        self.offset_label = Label(self.options, text="Offset:")
+        self.offset_label.grid(row=0, column=1, padx=20, sticky=E)
 
-        self.encode_decode_option = StringVar()
+        self.offset_entry = Entry(self.options, width=10, textvariable=self.offset)
+        self.offset_entry.grid(row=0, column=2, sticky=W)
 
-        self.encode_button = Radiobutton(self.options, text="Encode", variable=self.encode_decode_option, value="encode")
-        self.encode_button.grid(row=1, column=2, padx=(10,0))
+        self.keyword = StringVar()
 
-        self.decode_button = Radiobutton(self.options, text="Decode", variable=self.encode_decode_option, value="decode")
-        self.decode_button.grid(row=1, column=3, padx=(0,10))
+        self.keyword_label = Label(self.options, text="Keyword:")
+        self.keyword_label.grid(row=1, column=1, padx=20, pady=(0,10), sticky=E)
 
-        self.cypher_button = Button(self.options, text="Cypher Message!", command=lambda: self.cypher_options(self.cypher_option.get()))
-        self.cypher_button.grid(row=0, column=4, padx=(10,0), pady=(20,0), ipady=10)
+        self.keyword_entry = Entry(self.options, width=10, textvariable=self.keyword)
+        self.keyword_entry.grid(row=1, column=2, pady=(0,10), sticky=W)
+        self.keyword.set("friends")
 
-        self.clear_field_button = Button(self.options, text="Clear Fields", command=self.clear_fields)
-        self.clear_field_button.grid(row=0, column=6, padx=10, pady=(20,0), ipady=10, sticky=W)
+        self.encode_decode = StringVar()
 
-        self.cypher_option.set("caesar")
-        self.encode_decode_option.set("encode")
-        self.offset_keyword_entry.insert (0, "1")
+        self.encode_button = Radiobutton(self.options, text="Encode", variable=self.encode_decode, value="encode")
+        self.encode_button.grid(row=0, column=3, padx=(25,20), pady=(10,0))
+        self.encode_button.select()
 
-        self.bind("<Configure>", self.save_window_position)
+        self.decode_button = Radiobutton(self.options, text="Decode", variable=self.encode_decode, value="decode")
+        self.decode_button.grid(row=1, column=3, padx=(25,20), pady=(0,10))
 
+        self.cypher_button = Button(self.options, text="Cypher Message !", height=2, command=lambda: self.cypher_message(self.button_options.get()))
+        self.cypher_button.grid(row=0, column=4, rowspan=2, padx=15)
 
-    def save_window_position(self, event):
+        self.reset_button = Button(self.options, text="Reset", height=2, command=self.reset_fields)
+        self.reset_button.grid(row=0, column=5, rowspan=2, padx=15, sticky=W)
 
-        self.config["position_x"] = str(self.winfo_x())
-        self.config["position_y"] = str(self.winfo_y())
+        # Quit program button
+        self.close_button = Button(self, text="Close Application", command=self.destroy)
+        self.close_button.grid(row=2, column=0, columnspan=5, pady=10, ipadx=298, ipady=4)
 
-        print (self.config)
-
-        with open('app.config', 'w') as json_file:
-            json.dump(self.config, json_file)
-
-
-    def center_window (self, width, height):
-
-        print ("center_window")
-
-        screen_width = self.winfo_screenwidth ()
-        screen_height = self.winfo_screenheight ()
-
-        position_x = (screen_width - width)//2
-        position_y = (screen_height - height)//2
-
-        self.geometry (f"{width}x{height}+{position_x}+{position_y}")
+        self.reset_fields ()
 
 
-    def clear_fields(self):
+    def reset_fields(self):
 
         self.input_box.delete(1.0, "end")
         self.output_box.delete(1.0, "end")
 
-        # self.offset_keyword_entry.delete(0, END)
+        self.offset_entry.delete(0, END)
+        self.offset_entry.insert(0, "0")
 
-        # self.caesar_button.deselect()
-        # self.vigenere_button.deselect()
+        self.keyword_entry.delete(0, END)
+        self.keyword.set("friends")
 
-        # self.encode_button.deselect()
-        # self.decode_button.deselect()
+        self.caesar_button.select()
+        self.vigenere_button.deselect()
+
+        self.encode_button.select()
+        self.decode_button.deselect()
+
+        self.disable_keyword ()
 
 
-    def cypher_options(self, options):
+    def disable_keyword(self):
 
-        if debug or trace: print(f"cypher_options: {options}")
+        self.offset_entry.config(state=NORMAL)
+        self.keyword_entry.config(state=DISABLED)
+
+
+    def disable_offset(self):
+
+        self.keyword_entry.config(state=NORMAL)
+        self.offset_entry.config(state=DISABLED)
+
+
+    def cypher_message(self, options):
+
+        if debug: print("cypher_message()")
 
         if options == "caesar":
+            if trace: print("calling caesar_cypher()")
 
-            input_message = self.input_box.get("1.0", END)
-            offset = self.offset_keyword_entry.get()
-            coding_direction = self.encode_decode_option.get()
-
-            if trace: print(f"input_message: '{input_message}', offset: '{offset}', coding_direction: '{coding_direction}'")
-
-            if coding_direction == "":
-                self.update_results ("ERROR: must selection coding method")
-                return
-
-            # check if offset is all digits before continuing or 
+            message = self.input_box.get("1.0", END).strip()
+            action = self.encode_decode.get()
 
             try:
-                offset = int(offset)
+                offset = int(self.offset_entry.get())
 
             except:
-                if trace: print ("ERROR: string offset has nonnumeric characters")
-
-                self.update_results ("ERROR: string offset has nonnumeric characters")
+                messagebox.showerror("showerror", "ERROR: Offset value must be an integer")
                 return
 
-            self.update_results (caesar_cypher(input_message, offset, coding_direction))
+            if trace: print("message - '{message}', offset = {offset}, action = '{action}'")
+
+            results = caesar_cypher(message, offset, action)
 
 
-        if options == "vigenere":
+        else:
+            if trace: print("calling vigenere_cypher()")
 
-            if trace: print("calling vigenere_option()")
+            message = self.input_box.get("1.0", END).strip()
+            action = self.encode_decode.get()
+            keyword = self.keyword_entry.get()
+
+            if not keyword.isalpha():
+                messagebox.showerror("showerror", "ERROR: Keyword must only contain alphabets")
+                return
         
-            self.update_results (vigenere_cypher(self.input_box.get("1.0", END), self.offset_keyword_entry.get(), self.encode_decode_option.get()))
+            results = vigenere_cypher(message, keyword, action)
+
+        self.display_results(results)
 
 
-    def update_results (self, results):
-
+    def display_results(self, results):
         self.output_box.delete(1.0, "end")
         self.output_box.insert(1.0, results)
 
 
-def caesar_cypher(message, offset, method):
+def caesar_cypher(message, offset, action):
+    if debug: print(f"caesar_cypher() with offset - {offset}")
 
-    if debug or trace: print(f"initialized caesar_cypher() with message - '{message}' and offset - {offset}")
-    
-    message = message.lower()
+    message_copy = message.lower().strip()
 
-    new_message = ""
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    if debug: print(f"message_copy = '{message_copy}'")
+
+    if action == "decode":
+        offset = -(offset)
+
+    new_message = []
+
+    uppercase_letters = "ABVDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowercase_letters = "abcdefghijklmnopqrstuvwxyz"
+
+    all_letters = uppercase_letters + lowercase_letters
 
     for letter in message:
 
-        if trace: print(f"iterating through '{letter}' in message")
+        if trace: print(f"iterating through '{letter}' in message_copy")
 
-        if letter in alphabet:
-            old_index = alphabet.index(letter)
-
-            if trace: print(f"old_index = {old_index}")
-
-            if method == "encode":
-                new_index = old_index + (offset % 26)
-
-                if new_index > 25:
-                    new_index -= 26
-
-            if method == "decode":
-                new_index = old_index - (offset % 26)
-
-                if new_index < 0:
-                    new_index += 26
-
-            if trace: print(f"new_index = {new_index}")
-
-            new_message += alphabet[new_index]
-
-            if trace: print(f"new_message = {new_message}")
+        if not letter in all_letters:
+            new_message.append(letter)
 
         else:
-            new_message += letter
+            # old_index = alphabet.index(letter)
 
-    if debug or trace: print(f"new_message = '{new_message}'")
+            if letter.isupper ():
+                old_index = ord (letter) - ord('A')
+            else:
+                old_index = ord (letter) - ord('a')
 
-    return new_message
-
-
-def vigenere_cypher(message, keyword, method):
-
-    if debug or trace: print(f"initialized vigenere_cypher() with message - '{message}' and keyword - '{keyword}' and method '{method}'")
-
-    message = message.lower()
-
-    letters = "abcdefghijklmnopqrstuvwxyz"
-    key_index = 0
-
-    new_message = ""
-
-    for i in range(len(message)):
-
-        if trace: print(message[i])
+            if trace: print(f"old_index = {old_index}")
             
-        if not message[i].isalpha():
-            new_message += message[i]
-            continue
-        
-        if trace: print(f"key_index='{key_index}'")
-        
-        if method == "encode":
+            new_index = old_index + (offset % 26)
 
-            if trace: print(f"encode_decode_option.get() = '{method}'")
-
-            new_index = letters.index(message[i]) + letters.index(keyword[key_index])
-        
             if new_index > 25:
                 new_index -= 26
-
-        if method == "decode":
-            new_index = letters.index(message[i]) - letters.index(keyword[key_index])
 
             if new_index < 0:
                 new_index += 26
 
-        new_message += letters[new_index]
+            if trace: print(f"new_index = {new_index}")
 
-        key_index += 1
+            if letter.isupper ():
+                new_message.append(uppercase_letters[new_index])
+            else:
+                new_message.append(lowercase_letters[new_index])
 
-        if key_index >= len(keyword):
-            key_index = key_index % len(keyword)
 
-        if trace: print(f"new_message = {new_message}")
+            if trace: print(f"new_message = {new_message}")
 
-        
-    if debug or trace: print(new_message)
+
+
+    # for i in range(len(message)):
+
+    #     if debug: print(f"string[i] - '{message[i]}'")
+
+    #     if message[i].isupper():
+    #         new_message[i] = new_message[i].upper()
+            
+    #         if debug: print(f"new_message[i] - '{new_message[i]}'")
+
+    new_message = "".join(new_message)
+
+    if debug: print(f"new_message = '{new_message}'")
 
     return new_message
 
 
+def vigenere_cypher(message, keyword, action):
+
+    if debug: print(f"vigenere_cypher() with keyword -'{keyword}'")
+
+    message_copy = message.lower().strip()
+    keyword = keyword.lower()
+
+    if debug: print(f"message - '{message}', keyword - '{keyword}', action - '{action}'")
+
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    keyword_index = 0
+
+    new_message = []
+    indexes = []
+    
+    for letter in keyword:
+
+        if action == "encode":
+            indexes.append(letters.index(letter))        
+        else:
+            indexes.append(-(letters.index(letter)))
+
+    for i in range(len(message_copy)):
+
+        if trace: print(message_copy[i])
+            
+        if not message_copy[i].isalpha():
+            new_message.append(message_copy[i])
+            continue
+        
+        new_index = letters.index(message_copy[i]) + indexes[keyword_index]
+
+        if new_index > 25:
+            new_index -= 26
+
+        if new_index < 0:
+            new_index += 26
+
+        if trace: print(f"letters.index(message_copy[i]) = '{letters.index(message_copy[i])}', indexes[i % len(keyword) = '{indexes[i % len(keyword)]}', new_index - '{new_index}', {letters[new_index]}")
+
+        new_message.append(letters[new_index])
+
+        keyword_index += 1
+
+        if keyword_index >= len(keyword):
+            keyword_index = keyword_index % len(keyword)
+
+    for i in range(len(message)):
+
+        if debug: print(f"message[i] - '{message[i]}'")
+
+        if message[i].isupper():
+            new_message[i] = new_message[i].upper()
+            
+            if debug: print(f"new_message[i] - '{new_message[i]}'")
+
+    new_message = "".join(new_message)
+        
+    if debug: print(f"new_message - '{new_message}'")
+        
+    return new_message
+        
+
 if __name__ == "__main__":
 
-    application = Cypher_Application(title="Cypher GUI", width=775, height=810)
+    app = Cypher_Application(title="Cypher GUI", width=775, height=810)
 
-    try:
-        with open('app.config', 'r') as f:
-            application.config = json.load(f)
-            geometry = f"{application.config['width']}x{application.config['height']}+{application.config['position_x']}+{application.config['position_y']}"
-            
-            application.geometry (geometry)
-
-    except:
-        application.center_window (775, 810)
-
-        application.config = {
-            "position_x": "10",
-            "position_y": "10",
-            "width":"775",
-            "height":"810",
-            "keyword":"friends",
-            "offset":"1"
-        }
-
-
-    application.mainloop()
-
-    with open('app.config', 'w') as json_file:
-        json.dump(application.config, json_file)
+    app.mainloop()
